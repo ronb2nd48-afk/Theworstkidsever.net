fed156a93ab613436a043d813e325f21c1db5d58
ronb2nd48-afk <ronb2nd48@icloud.com>
2025-12-12 06:57:12 -0800

    Add Firebase initialization script

---
+++ Firebase.js
@@ -0,0 +1,17 @@
+<!-- firebase.js -->
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+  const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "XXXX",
+    appId: "XXXX"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  window.auth = getAuth(app);
+</script>
